f80e025cbf2c7ae8decbcaa16c9deae8bc450a2b
Cr0ssEyE <hangman0117@gmail.com>
2023-09-24 13:44:00 +0900

    [#9] BT 기본 구조 작업용 클래스 생성
    무엇이 필요한지만 정리했고, 세부적인 내용 구현을 이어 진행합니다.

---
+++ Source/XR_Project_Team10/Character/Monster/Common/BTService_CommonDetect.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTService.h"
+#include "BTService_CommonDetect.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class XR_PROJECT_TEAM10_API UBTService_CommonDetect : public UBTService
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/XR_Project_Team10/Character/Monster/Common/BTTask_KWCommonFindLocation.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "BTTask_KWCommonFindLocation.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class XR_PROJECT_TEAM10_API UBTTask_KWCommonFindLocation : public UBTTaskNode
+{
+	GENERATED_BODY()
+	
+};
