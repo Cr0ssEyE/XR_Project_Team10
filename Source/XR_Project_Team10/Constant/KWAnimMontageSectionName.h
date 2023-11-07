ef56fbe7f16f952dc70753d492057eaf0b10115d
LEESANGYUN <lhyun6551@naver.com>
2023-11-08 01:29:45 +0900

    [#7] 렙디 병합 전 임시
    .

--- Source/XR_Project_Team10/Constant/KWAnimMontageSectionName.h
+++ Source/XR_Project_Team10/Constant/KWAnimMontageSectionName.h
@@ -7,3 +7,8 @@
 #define SECTION_MELEE_ATTACK TEXT("MeleeAttack")
 #define SECTION_BACK_STEP TEXT("BackStep")
 #define SECTION_WHIRL_WIND TEXT("Whirlwind")
+
+// 피격/사망 공용 애님 몽타주 섹션
+#define SECTION_HIT TEXT("Hit")
+#define SECTION_DEAD TEXT("Dead")
+#define SECTION_DEAD_LOOP TEXT("Dead_Loop")
