d509b2f82ed1e4adfe519cad19964677a90aab00
Cr0ssEyE <hangman0117@gmail.com>
2023-11-22 20:53:47 +0900

    [#45] 기어 변경 표시 위젯 작업중
    .

--- Source/XR_Project_Team10/UI/KWPlayerGearWidget.cpp
+++ Source/XR_Project_Team10/UI/KWPlayerGearWidget.cpp
@@ -3,3 +3,76 @@
 
 #include "XR_Project_Team10/UI/KWPlayerGearWidget.h"
 
+void UKWPlayerGearWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+	NewGearState = EGearState::EndOfGearState;
+
+	if(!AnimationChangeSpeed)
+	{
+		AnimationChangeSpeed = 0.04f;
+	}
+	CurrentAnimationState = 0;
+	bIsAnimationOnGoing = false;
+	NewGearImage->SetRenderScale(FVector2d::Zero());
+	TestBtn->OnClicked.AddDynamic(this, &UKWPlayerGearWidget::AnimTestFunction);
+}
+
+
+void UKWPlayerGearWidget::ApplyPlayerGearState(EGearState GearState)
+{
+	if(bIsAnimationOnGoing)
+	{	
+		return;
+	}
+	NewGearState = GearState;
+	NewGearImage->SetRenderScale(FVector2d::One());
+	GetWorld()->GetTimerManager().SetTimer(GearAnimationTimerHandle, this, &UKWPlayerGearWidget::PlayGearChangeAnimation, AnimationChangeSpeed, true);
+}
+
+
+void UKWPlayerGearWidget::PlayGearChangeAnimation()
+{
+	CurrentAnimationState++;
+	UTexture2D* NextTexture2D = nullptr;
+	switch (NewGearState)
+	{
+	case EGearState::GearOne:
+		NextTexture2D = GearOneImages[CurrentAnimationState];
+		break;
+	case EGearState::GearTwo:
+		NextTexture2D = GearTwoImages[CurrentAnimationState];
+		break;
+	case EGearState::GearThree:
+		NextTexture2D = GearThreeImages[CurrentAnimationState];
+		break;
+	case EGearState::GearFour:
+		NextTexture2D = GearFourImages[CurrentAnimationState];
+		break;
+	default:
+		checkNoEntry();
+	}
+	if(!GearOneImages[CurrentAnimationState + 1])
+	{
+		CurrentGearImage->SetBrushFromTexture(NextTexture2D);
+		NewGearImage->SetBrushFromTexture(nullptr);
+		NewGearImage->SetRenderScale(FVector2d::Zero());
+		bIsAnimationOnGoing = false;
+		CurrentAnimationState = 0;
+		
+		GetWorld()->GetTimerManager().ClearTimer(GearAnimationTimerHandle);
+		return;
+	}
+	NewGearImage->SetBrushFromTexture(NextTexture2D);
+}
+
+void UKWPlayerGearWidget::AnimTestFunction()
+{
+	if(NewGearState != EGearState::EndOfGearState)
+	{
+		int NextGear = static_cast<int>(NewGearState) + 1;
+		NewGearState = static_cast<EGearState>(NextGear);
+		NewGearImage->SetRenderScale(FVector2d::One());
+	}
+	GetWorld()->GetTimerManager().SetTimer(GearAnimationTimerHandle, this, &UKWPlayerGearWidget::PlayGearChangeAnimation, AnimationChangeSpeed, true);
+}
--- Source/XR_Project_Team10/UI/KWPlayerGearWidget.h
+++ Source/XR_Project_Team10/UI/KWPlayerGearWidget.h
@@ -3,7 +3,9 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "Components/Image.h"
 #include "Blueprint/UserWidget.h"
+#include "XR_Project_Team10/Player/KWPlayerCharacter.h"
 #include "KWPlayerGearWidget.generated.h"
 
 /**
@@ -13,5 +15,54 @@ UCLASS()
 class XR_PROJECT_TEAM10_API UKWPlayerGearWidget : public UUserWidget
 {
 	GENERATED_BODY()
+
+public:
+	void ApplyPlayerGearState(EGearState GearState);
+
+protected:
+	virtual void NativeConstruct() override;
+	void PlayGearChangeAnimation();
 	
+	UFUNCTION(BlueprintCallable)
+	void AnimTestFunction();
+	
+protected:
+	UPROPERTY(EditDefaultsOnly, Category = "UI", meta = (BindWidget))
+	TObjectPtr<UImage> CurrentGearImage;
+
+	UPROPERTY(EditDefaultsOnly, Category = "UI", meta = (BindWidget))
+	TObjectPtr<UImage> NewGearImage;
+
+	UPROPERTY(EditDefaultsOnly, Category = "UI")
+	TArray<UTexture2D*> KiwiModeImages;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "UI")
+	TArray<UTexture2D*> GearOneImages;
+
+	UPROPERTY(EditDefaultsOnly, Category = "UI")
+	TArray<UTexture2D*> GearTwoImages;
+
+	UPROPERTY(EditDefaultsOnly, Category = "UI")
+	TArray<UTexture2D*> GearThreeImages;
+
+	UPROPERTY(EditDefaultsOnly, Category = "UI")
+	TArray<UTexture2D*> GearFourImages;
+
+	UPROPERTY(EditDefaultsOnly, Category = "UI", meta=(BindWidget))
+	TObjectPtr<UButton> TestBtn;
+
+private:
+	EGearState NewGearState;
+
+	UPROPERTY(EditAnywhere)
+	float AnimationChangeSpeed;
+	
+	UPROPERTY()
+	int CurrentAnimationState;
+
+	UPROPERTY()
+	uint8 bIsAnimationOnGoing : 1;
+	
+	UPROPERTY()
+	FTimerHandle GearAnimationTimerHandle;
 };
